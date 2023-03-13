b848f1fa1cc0667a8e7253b64b0c6674bccaefa5
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-03-13 19:39:06 -0400

    Fixed the getdb and putdb i THINK

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,33 +7,33 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: false });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {  
-  console.log('Posting...');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
-  const result = await request;
-  console.log('Data saved to database!', result)
-  return result;
+// Add text to indexedDB
+export const putDb = async (content) => {
+  console.log("Updating data...")
+	const jateDB = await openDB('jate', 1);
+	const tx = jateDB.transaction('jate', 'readwrite');
+	const store = tx.objectStore('jate');
+	const request = store.put({ id: 1, content: content });
+	const result = await request;
+	console.log("Data updated!", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Retrieve text from indexedDB
 export const getDb = async () => {
-  console.log('GET-ing everything in database...');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.getAll();
-  const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log("Retrieving data...")
+	const jateDb = await openDB('jate', 1);
+	const tx = jateDb.transaction('jate', 'readonly');
+	const store = tx.objectStore('jate');
+	const request = store.get(1);
+	const result = await request;
+  console.log("Data Retrieved!", result)
+	return result?.value;
 };
 
 initdb();
