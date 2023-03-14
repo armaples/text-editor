1a7cff59db4db78bd5df2cb5e7683413d975d8b3
Alyssa <112567857+armaples@users.noreply.github.com>
2023-03-13 20:24:14 -0400

    Update database.js
    Added more comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,9 +16,13 @@ const initdb = async () =>
 // Updating the data with indexedDB
 export const putDb = async (content) => {
   	console.log("Updating data...")
+	// Creates a connection to database & version to be used
 	const jateDB = await openDB('jate', 1);
+	// Creates new "transaction" and specifies database and what someone can do with that data (in this case, possible to read and edit)
 	const tx = jateDB.transaction('jate', 'readwrite');
+	// Opens the object store
 	const store = tx.objectStore('jate');
+	// uses .put to store content with an associated id
 	const request = store.put({ id: 1, content: content });
 	const result = await request;
 	console.log("Data updated!", result);
@@ -27,12 +31,17 @@ export const putDb = async (content) => {
 // Retrieving the data with indexedDB
 export const getDb = async () => {
   	console.log("Retrieving data...")
+	// Creates a connection to database & version to be used
 	const jateDb = await openDB('jate', 1);
+	// Creates new "transaction" and specifies database and what someone can do with that data (in this case, it is only possible to read)
 	const tx = jateDb.transaction('jate', 'readonly');
+	// Opens the object store
 	const store = tx.objectStore('jate');
+	// uses .get to get content with the associated id
 	const request = store.get(1);
 	const result = await request;
   	console.log("Data Retrieved!", result)
+	// returns only the value of the result (not the id!). ? helps in case the value is undefined
 	return result?.value;
 };
 
